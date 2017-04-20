1ae1a1b569099feea696309e82e9cbf7bac8d242
Ashwini <ashie.oh@gmail.com>
2017-04-20 18:45:15 +0100

    Add one feature spec

---
+++ spec/FeatureSpec.js
@@ -0,0 +1,20 @@
+describe("Game", function() {
+  var player1 = new Player('X');
+  var player2 = new Player('O');
+  var grid = new Grid({rows:3, columns:3});
+  var game = new Game(player1, player2, grid);
+
+  it("", function(){
+    game.play(player1,{row:0, col:0});
+    game.play(player2,{row:0, col:1});
+    game.play(player1,{row:0, col:2});
+    game.play(player2,{row:1, col:0});
+    game.play(player1,{row:1, col:1});
+    game.play(player2,{row:2, col:0});
+    game.play(player1,{row:1, col:2});
+    game.play(player2,{row:2, col:2});
+    game.play(player1,{row:2, col:1});
+    expect(game.isOver()).toBe(true);
+  });
+
+});
