bf7ed2a0c182aea4425302d2a81b59528cca406b
Ashwini <ashie.oh@gmail.com>
2017-04-20 16:41:12 +0100

    Test drive creation of methods for game know when its over

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,9 +1,11 @@
 describe("Game", function(){
+  var player1, player2, game, grid
 
   beforeEach(function(){
     player1 = {name: 'X'};
     player2 = {name: 'O'};
-    game = new Game(player1, player2);
+    grid = new Grid({rows: 3, columns: 3});
+    game = new Game(player1, player2, grid);
   });
 
   it("#new", function(){
@@ -12,6 +14,18 @@ describe("Game", function(){
 
   it("#players", function(){
     expect(game.playerList()).toEqual([player1, player2]);
+  });
+
+  it("#platform", function(){
+    expect(game.platform()).toEqual(grid);
+  });
+
+  it("#isWon", function(){
+    expect(game.isWon()).toBe(false);
+  });
+
+  it("#isOver", function(){
+    expect(game.isOver()).toBe(false);
   });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,29 @@
 (function(exports){
 
-  function Game(player1, player2) {
+  function Game(player1, player2, grid) {
+    this.currentPlayer = player1;
+    this.opponentPlayer = player2;
     this.players = [player1, player2];
+    this.grid = grid;
   }
 
   Game.prototype.playerList = function () {
     return this.players;
+  };
+
+  Game.prototype.platform = function () {
+    return this.grid;
+  };
+
+  Game.prototype.isWon = function () {
+    var won = this.grid.hasPlayerClaimedRow(this.currentPlayer) || this.grid.hasPlayerClaimedColumn(this.currentPlayer) || this.grid.hasPlayerClaimedDiagonal(this.currentPlayer) ||
+      this.grid.hasPlayerClaimedRow(this.opponentPlayer) || this.grid.hasPlayerClaimedColumn(this.opponentPlayer) || this.grid.hasPlayerClaimedDiagonal(this.opponentPlayer);
+    return won;
+  };
+
+  Game.prototype.isOver = function () {
+    var over = this.grid.areAllFieldsCalimed()
+    return over;
   };
 
   exports.Game = Game;
